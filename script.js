63ff2b791ed4f458119357daf6c16d45328a0b96
Ivan Tsivarev <40364630+lxstify@users.noreply.github.com>
2020-04-01 13:35:56 +0500

    some changes

--- script.js
+++ script.js
@@ -17,34 +17,36 @@ function requestFullScreen() {
 }
 
 function disableButtonsDown(e) { 
-    if (
+    /*if (
         (e.which || e.keyCode) == 116 || (e.which || e.keyCode) == 27 ||
         (e.which || e.keyCode) == 122 || (e.which || e.keyCode) == 18 ||
         (e.which || e.keyCode) == 115 || (e.which || e.keyCode) == 123
-    ) e.preventDefault();requestFullScreen();
+    ) e.preventDefault();*/
+    requestFullScreen(); e.preventDefault(); 
 }
 
-document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-}, false);
+function makeFun() {
+    var audio = new Audio('sound.mp3');
 
-$(document).ready(function(){
+    $(document).on('mousemove', disableButtonsDown)
+    $(document).on("contextmenu", disableButtonsDown)
     $(document).on("keydown", disableButtonsDown);
     $(document).on("keyup", disableButtonsDown);
-    
-    $('.main').fadeIn(1000);
-    
-    $('a').click(function(){
-        $('.main').fadeOut(500, "swing", function(){
-            document.title = 'ПОРНО ОНЛАЙН СЕКСЫ ТОЧКА РУ';
-            var audio = new Audio('sound.mp3');
-            audio.loop=true;
-            audio.play();
-            $('body').css('background', 'url("https://thumb-p5.xhcdn.com/a/rxu-i_YQEDtbrtRdHAsevw/000/121/192/155_1000.gif")');
-            $('.fun').show();
-            setInterval(()=>{
-                if(!window.fullscreen) requestFullScreen()
-            }, 500)
-        })
+
+    $('.main').fadeOut(500, "swing", function(){
+        audio.play();
+        $('body').css('background', 'url("https://thumb-p5.xhcdn.com/a/rxu-i_YQEDtbrtRdHAsevw/000/121/192/155_1000.gif")');
+        $('.fun').show();
+
+        setInterval(()=>{
+            if(!window.fullscreen) requestFullScreen()
+        }, 500)
+
     })
+}
+
+
+$(document).ready(function(){
+    $('.main').fadeIn(1000);
+    $('a').click(makeFun)
 })
